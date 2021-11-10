f56391798977c22cee532281e33d568a1b97e80c
Arnaud <arnaud.mercier.1@ulaval.ca>
2021-11-10 14:59:03 -0500

    Impemented a basic graph and a basic save function

--- test_python.py
+++ test_python.py
@@ -1,7 +1,10 @@
+import matplotlib.pyplot as plt
+import numpy as np
 import serial
 import fnmatch
 import os
 import re
+import matplotlib.pyplot as pl
 
 
 
@@ -41,25 +44,47 @@ while ON:
             files.remove('{}.txt'.format(name))
             num = '(%i)' % (int(re.search(r'\(([0-9]+)\)', max(files)).group(1)) + 1)
 
-        output_file = open('{}{}.txt'.format(name, num), 'w')
 
 
         ser.write("{} 1".format(command).encode())
         line = ser.read_until('packets : '.encode())
         numberOfSample = int(ser.read_until().decode("utf-8"))
-        for i in range(numberOfSample-3):
+        temps1 = np.zeros((numberOfSample-5))
+        seis1 = np.zeros((numberOfSample-5))
+        for i in range(numberOfSample-5):
             line = ser.readline()
             line = line.decode("utf-8")
             if line.count(',') == 1:
-                output_file.write(line)
+                # output_file.write(line)
+                temp = line.split(',')
+                temps1[i] = int(temp[0])
+                seis1[i] = int(temp[1])
+
+
+
         ser.write("{} 2".format(command).encode())
         line = ser.read_until('packets : '.encode())
         numberOfSample = int(ser.read_until().decode("utf-8"))
-        for i in range(numberOfSample - 3):
+        temps2 = np.zeros((numberOfSample - 5))
+        seis2 = np.zeros((numberOfSample - 5))
+        for i in range(numberOfSample - 5):
             line = ser.readline()
             line = line.decode("utf-8")
             if line.count(',') == 1:
-                output_file.write(line)
+                # output_file.write(line)
+                temp = line.split(',')
+                temps2[i] = int(temp[0])
+                seis2[i] = int(temp[1])
+
+
+        plt.plot(temps1, seis1, label="ADC 1")
+        plt.plot(temps2, seis2, label="ADC 2")
+        plt.show()
+
+    elif command == 'save':
+        output_file = open('{}{}.txt'.format(name, num), 'w')
+        for i, sample in enumerate(temps1):
+            output_file.write("{},{},{},{} \n".format(sample, seis1[i],temps2[i],seis2[i]))
 
         output_file.close()
         print('file close')
