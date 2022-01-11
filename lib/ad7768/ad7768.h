6c39ef3a08537b7ea09f45f91901c0f3d2935451
Arnaud <arnaud.mercier.1@ulaval.ca>
2022-01-11 13:41:10 -0500

    commented librairy file and added GIGul image

--- lib/ad7768/ad7768.h
+++ lib/ad7768/ad7768.h
@@ -1,40 +1,14 @@
 /***************************************************************************//**
  *   @file   ad7768.h
  *   @brief  Header file of AD7768 Driver.
- *   @author DBogdan (dragos.bogdan@analog.com)
+ *   @author armercier (arnaud.mercier.1@ulaval.ca)
 ********************************************************************************
- * Copyright 2016(c) Analog Devices, Inc.
- *
- * All rights reserved.
- *
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions are met:
- *  - Redistributions of source code must retain the above copyright
- *    notice, this list of conditions and the following disclaimer.
- *  - Redistributions in binary form must reproduce the above copyright
- *    notice, this list of conditions and the following disclaimer in
- *    the documentation and/or other materials provided with the
- *    distribution.
- *  - Neither the name of Analog Devices, Inc. nor the names of its
- *    contributors may be used to endorse or promote products derived
- *    from this software without specific prior written permission.
- *  - The use of this software may or may not infringe the patent rights
- *    of one or more patent holders.  This license does not release you
- *    from the requirement that you obtain separate licenses from these
- *    patent holders to use this software.
- *  - Use of the software either in source or binary form, must be run
- *    on or directly connected to an Analog Devices Inc. component.
- *
- * THIS SOFTWARE IS PROVIDED BY ANALOG DEVICES "AS IS" AND ANY EXPRESS OR
- * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, NON-INFRINGEMENT,
- * MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
- * IN NO EVENT SHALL ANALOG DEVICES BE LIABLE FOR ANY DIRECT, INDIRECT,
- * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
- * LIMITED TO, INTELLECTUAL PROPERTY RIGHTS, PROCUREMENT OF SUBSTITUTE GOODS OR
- * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
- * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
- * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
- * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+This librairy is adapted from the original ad7768 librairy from Analog device 
+https://github.com/analogdevicesinc/no-OS/blob/master/drivers/adc/ad7768/ad7768.c
+
+It integrates the SPI communication protocol of the arduino instead of the standard
+generic SPI protocol. Base functions to configure and read the configuration of 
+ADC.
 *******************************************************************************/
 #ifndef AD7768_H_
 #define AD7768_H_
@@ -44,8 +18,7 @@
 /******************************************************************************/
 #include <stdint.h>
 #include <Stream.h>
-// #include "gpio.h"
-// #include "spi.h"
+
 
 /******************************************************************************/
 /********************** Macros and Constants Definitions **********************/
